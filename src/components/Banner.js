ac46be5bb6d60d5bb5a08b32a460dcdad55589f7
unknown <mdtouhidul625@gmail.com>
2022-05-17 00:46:48 +0600

    i

---
+++ src/components/Banner.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div><div className="max-w-7xl px-4 py-2 mx-auto sm:px-6 lg:px-8">
+        <div className="max-w-lg mx-auto">
+          <h1 className="text-2xl font-bold text-center text-indigo-600 sm:text-3xl">
+            Get started today
+          </h1>
+          <form className="p-8 mt-1 mb-0 space-y-4 rounded-lg shadow-2xl">
+            <p className="text-lg font-medium">
+              Please provide below information to connect
+            </p>
+
+            <div>
+              <label className="text-md font-medium">Name</label>
+
+              <div className="relative mt-1">
+                <input
+                 
+                  type="text"
+                  id="name"
+                  className="w-full p-4 pr-12 text-md border-gray-200 rounded-lg shadow-md"
+                  placeholder="Enter your name"
+                />
+              </div>
+            </div>
+            <div>
+              <label className="text-sm font-medium">Email</label>
+
+              <div className="relative mt-1">
+                <input
+                 
+                  type="email"
+                  id="email"
+                  className="w-full p-4 pr-12 text-md border-gray-200 rounded-lg shadow-md"
+                  placeholder="Enter email"
+                />
+              </div>
+            </div>
+            <div>
+              <label className="text-md font-medium">Address</label>
+
+              <div className="relative mt-1">
+                <input
+                 
+                  type="text"
+                  id="birthday"
+                  className="w-full p-4 pr-12 text-md border-gray-200 rounded-lg shadow-md"
+                  placeholder="Enter you Address"
+                />
+              </div>
+            </div>
+            <div>
+              <label className="text-md font-medium">Profession</label>
+
+              <div className="relative mt-1">
+                <input
+                 
+                  type="text"
+                  id="profession"
+                  className="w-full p-4 pr-12 text-md border-gray-200 rounded-lg shadow-md"
+                  placeholder="Enter your profession"
+                />
+              </div>
+            </div>
+
+            <button
+              
+              type="submit"
+              className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg active:bg-indigo-800"
+            >
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+</div>
+  )
+}
+
+export default Banner
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from '../assets/images/1.png'
+
+function Header() {
+  return (
+    <div><header className="max-w-7xl mx-auto bg-indigo-200 flex items-center justify-between p-5 sticky top-0 z-50">
+        <img
+          className="w-44 object-contain cursor-pointer"
+          src={logo}
+        />
+
+        <div className="hidden md:inline-flex items-center space-x-2 font-semibold">
+         
+            <h3 className="hover:bg-indigo-400 transform active:bg-indigo-600 duration-200 ease-in-out px-6 cursor-pointer py-2 rounded-md">
+              Home
+            </h3>
+          
+
+          
+            <h3 className="hover:bg-indigo-400 transform active:bg-indigo-600 duration-200 ease-in-out px-6 cursor-pointer py-2 rounded-md">
+              User Data
+            </h3>
+         
+
+          <h3 className="hover:bg-indigo-400 transform active:bg-indigo-600 duration-200 ease-in-out px-6 cursor-pointer py-2 rounded-md">
+            Summary
+          </h3>
+        </div>
+        <div className="flex items-center space-x-2 text-Zinc-900">
+          <h3
+            className="border px-3 py-2 rounded-md border-indigo-600
+        hover:bg-indigo-400 transform active:bg-indigo-600 duration-200 ease-in-out cursor-pointer"
+          >
+            Get Started
+          </h3>
+        </div>
+      </header></div>
+  )
+}
+
+export default Header
